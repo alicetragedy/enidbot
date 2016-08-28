64b936fc593f6113f60c9dc1dff644fc3fa7b817
alicetragedy <girl@alicetragedy.org>
2016-08-28 14:25:11 +0200

    add moar enid images

--- moviemood.py
+++ moviemood.py
@@ -34,15 +34,58 @@ def inlinequery(bot, update):
 
   results.append(InlineQueryResultPhoto(id=uuid4(),
                                           type="photo",
-                                          title="ghost world",
+                                          title="drawing enid",
                                           photo_url="http://www.radioteos.ru/kadr/wp-content/uploads/2013/11/ghost-world.jpg",
                                           thumb_url="http://www.radioteos.ru/kadr/wp-content/uploads/2013/11/ghost-world.jpg",
                                           ))
 
   results.append(InlineQueryResultPhoto(id=uuid4(),
                                           type="photo",
-                                          photo_url="http://67.media.tumblr.com/tumblr_l3pbo60QXN1qzdstpo1_500.jpg",
-                                          thumb_url="http://67.media.tumblr.com/tumblr_l3pbo60QXN1qzdstpo1_500.jpg",
+                                          title="only stupid people",
+                                          photo_url="https://img.buzzfeed.com/buzzfeed-static/static/enhanced/webdr03/2013/3/19/16/enhanced-buzz-3056-1363724853-4.jpg",
+                                          thumb_url="https://img.buzzfeed.com/buzzfeed-static/static/enhanced/webdr03/2013/3/19/16/enhanced-buzz-3056-1363724853-4.jpg",
+                                          ))
+
+  results.append(InlineQueryResultPhoto(id=uuid4(),
+                                          type="photo",
+                                          title="punk rock enid",
+                                          photo_url="https://img.buzzfeed.com/buzzfeed-static/static/enhanced/webdr01/2013/3/19/16/enhanced-buzz-1962-1363723752-1.jpg",
+                                          thumb_url="https://img.buzzfeed.com/buzzfeed-static/static/enhanced/webdr01/2013/3/19/16/enhanced-buzz-1962-1363723752-1.jpg",
+                                          ))
+
+  results.append(InlineQueryResultPhoto(id=uuid4(),
+                                          type="photo",
+                                          title="so bad",
+                                          photo_url="https://65.media.tumblr.com/tumblr_m8dv6zyXIW1raezz2o1_500.jpg",
+                                          thumb_url="https://65.media.tumblr.com/tumblr_m8dv6zyXIW1raezz2o1_500.jpg",
+                                          ))
+
+  results.append(InlineQueryResultPhoto(id=uuid4(),
+                                          type="photo",
+                                          title="just disappear",
+                                          photo_url="https://24.media.tumblr.com/tumblr_me1g7wPYcc1rsyukao1_1280.jpg",
+                                          thumb_url="https://24.media.tumblr.com/tumblr_me1g7wPYcc1rsyukao1_1280.jpg",
+                                          ))
+
+  results.append(InlineQueryResultPhoto(id=uuid4(),
+                                          type="photo",
+                                          title="sick of everybody",
+                                          photo_url="https://41.media.tumblr.com/4c522a9a47efb66847759c08d503fe94/tumblr_nk6n8w7yel1rsyukao1_500.jpg",
+                                          thumb_url="https://41.media.tumblr.com/4c522a9a47efb66847759c08d503fe94/tumblr_nk6n8w7yel1rsyukao1_500.jpg",
+                                          ))
+
+  results.append(InlineQueryResultPhoto(id=uuid4(),
+                                          type="photo",
+                                          title="cannot relate",
+                                          photo_url="https://67.media.tumblr.com/tumblr_m8dv0iU7lT1raezz2o1_500.jpg",
+                                          thumb_url="https://67.media.tumblr.com/tumblr_m8dv0iU7lT1raezz2o1_500.jpg",
+                                          ))
+
+  results.append(InlineQueryResultPhoto(id=uuid4(),
+                                          type="photo",
+                                          title="sick of the weirdos",
+                                          photo_url="https://data.whicdn.com/images/124205910/large.jpg",
+                                          thumb_url="https://data.whicdn.com/images/124205910/large.jpg",
                                           ))
 
   bot.answerInlineQuery(update.inline_query.id, results=results)
